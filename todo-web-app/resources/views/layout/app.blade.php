af28a9c4e2449c22f6008a2ed852b4d7d8f5ba18
calvinhuynh14 <huynh.calvin@outlook.com>
2025-03-08 23:02:19 -0500

    - Set up project - Reused app layout from previous assignment - Created login form for the log in page

---
+++ todo-web-app/resources/views/layout/app.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <title>To-Do List App</title>
+</head>
+
+<body>
+    <header>
+        <h1 class="bg-primary text-white p-4">To-Do List App</h1>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a href="/" class="nav-link {{ request()->is('/') ? 'active' : '' }}">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a href="/manage" class="nav-link {{ request()->is('manage') ? 'active' : '' }}">Register</a>
+                </li>
+                <li class="nav-item">
+                    <a href="/search" class="nav-link {{ request()->is('search') ? 'active' : '' }}">Manage</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <main style="padding-bottom: 80px;">
+        @yield('content')
+
+    </main>
+    <footer class="bg-primary text-white text-center fixed-bottom">
+        <p>&copy; To-Do List App 2025</p>
+    </footer>
+</body>
+</html>
---
+++ todo-web-app/resources/views/login.blade.php
@@ -0,0 +1,25 @@
+@extends('layout.app')
+
+@section('content')
+    <div class="card" style="width: 50%; margin: 0 auto;">
+        <div class="card-header">
+            <h1>Login</h1>
+        </div>
+        <div class="card-body" style="padding: 20px;">
+            <form action="/login" method="post" class="form-group">
+                @csrf
+                <div class="form-group mb-3">
+                    <label for="email">Email</label>
+                    <input type="text" name="email" class="form-control" placeholder="john@doe.com">
+                </div>
+                <div class="form-group mb-3">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" class="form-control" placeholder="********">
+                </div>
+                <button type="submit" class="btn btn-primary">Login</button>
+            </form>
+        </div>
+    </div>
+@endsection
+
+
